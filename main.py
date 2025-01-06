55c078782e79ca4e787fa3c9abf25c4319399b1a
JavadiSM <1remehr@gmail.com>
2025-01-06 23:52:56 +0330

    enums created, code refactored

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
 import cv2  
 import numpy as np  
+import enums
 
-# Replace with your IP Webcam URL  
-url = 'http://192.168.1.4:8080/video'  
+def main(vid_cap,url):
+    while True:  
+        # Read a frame from the stream  
+        ret, frame = vid_cap.read()  
+        if not ret:  
+            print("Error: Could not read frame.")  
+            break  
 
-# Start capturing from the IP camera  
-cap = cv2.VideoCapture(url)  
+        # Perform image processing (example: convert to grayscale)  
+        show_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  
+        
+        # Display the resulting frames  
+        cv2.imshow('Original Frame', frame)  # Show the original frame  
+        cv2.imshow('Gray Frame', show_frame)  # Show the processed frame  
 
-if not cap.isOpened():  
-    print("Error: Could not open video stream.")  
-    exit()  
+        # Exit on 'q' key  
+        if cv2.waitKey(1) & 0xFF == enums.EXIT_KEY:  
+            break 
 
-while True:  
-    # Read a frame from the stream  
-    ret, frame = cap.read()  
-    if not ret:  
-        print("Error: Could not read frame.")  
-        break  
+def connect(ip_address):
+    url = ip_address
+    # Start capturing from the IP camera  
+    cap = cv2.VideoCapture(url)  
 
-    # Perform image processing (example: convert to grayscale)  
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  
-    
-    # Display the resulting frames  
-    cv2.imshow('Original Frame', frame)  # Show the original frame  
-    cv2.imshow('Gray Frame', gray_frame)  # Show the processed frame  
+    if not cap.isOpened():  
+        print("Yadet raft gooshio vasl koni dobare")  
+        exit()
+    return cap,url   
 
-    # Exit on 'q' key  
-    if cv2.waitKey(1) & 0xFF == ord('q'):  
-        break  
-
-# Release the video capture object and close all OpenCV windows  
-cap.release()  
-cv2.destroyAllWindows()
+if __name__ == '__main__':    
+    vid_cap,url = connect(enums.URL)
+    # Release the video capture object and close all OpenCV windows  
+    main(vid_cap,url)
+    vid_cap.release()  
+    cv2.destroyAllWindows()
