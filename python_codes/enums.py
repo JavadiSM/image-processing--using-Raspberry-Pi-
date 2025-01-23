2d33036e1fd96c23d318ceb903da9f68d747ef65
kasramo1383 <montazerikasra@gmail.com>
2025-01-23 19:49:39 +0330

    hide computer eye and live feed windows, full screen edge window, comment rescaling

--- python_codes/enums.py
+++ python_codes/enums.py
@@ -3,5 +3,5 @@ import cv2
 import numpy as np  
 class enums(Enum):
     # Replace with your IP Webcam URL  
-    URL = 'http://192.168.1.6:8080/video'  
+    URL = 'http://192.168.246.18:8080/video'  
     EXIT_KEY = ord('q')
--- python_codes/main.py
+++ python_codes/main.py
@@ -3,6 +3,9 @@ import numpy as np
 from enums import enums
 import time
 def main(vid_cap:cv2.VideoCapture, fps = 1) -> None:
+    cv2.namedWindow('edges', cv2.WINDOW_NORMAL)
+    cv2.setWindowProperty('edges', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    
     """
     # READ BEFORE USE
     this function receives each image every time,
@@ -20,7 +23,7 @@ def main(vid_cap:cv2.VideoCapture, fps = 1) -> None:
         blank = np.zeros(frame.shape, dtype='uint8')
         result = see_frame(frame)
         countoures,hierarchies = cv2.findContours(result,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-        cv2.imshow('computer eye',result)
+        # cv2.imshow('computer eye',result)
         
         cv2.drawContours(blank,countoures,-1,(0,255,0),5)
         cv2.imshow('edges',blank)
@@ -34,7 +37,8 @@ def see_frame(frame):
     """
     computer only tends to see lines, borders, rather than photo
     """
-    gray = rescale(frame)
+    # gray = rescale(frame)
+    gray = frame
     gray = cv2.cvtColor(gray,cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray,(7,7), cv2.BORDER_DEFAULT)
     ret, threshold = cv2.threshold(blur,125,255,cv2.THRESH_BINARY)
@@ -50,14 +54,14 @@ def exit_check(vid_cap):
         return True
     return False
 
-def rescale(frame, scale: float = 0.5) -> np.ndarray:  
+def rescale(frame, scale: float = 1) -> np.ndarray:  
     # Get dimensions of the original frame  
     height, width = frame.shape[:2]  
     
     # Calculate new dimensions  
     new_dims = (int(width * scale), int(height * scale))  
     
-    # Resize the frame  
+    # Resize the frame
     resized_frame = cv2.resize(frame, new_dims, interpolation=cv2.INTER_AREA)  
     
     return resized_frame 
@@ -71,7 +75,7 @@ def connect(ip_address) -> cv2.VideoCapture:
     if not cap.isOpened():
         print("Error: Could not open video capture.")
         exit()
-    cv2.namedWindow('Live Feed')
+    # cv2.namedWindow('Live Feed')
     return cap
 
 if __name__ == '__main__':
